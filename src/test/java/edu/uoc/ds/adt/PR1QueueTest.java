74c1c248cf77026c89f8c68a7293cd820f76c584
Leizan <leizan89@hotmail.com>
2025-02-25 10:56:03 +0100

    ejercicio2

--- src/test/java/edu/uoc/ds/adt/PR1QueueTest.java
+++ src/test/java/edu/uoc/ds/adt/PR1QueueTest.java
@@ -11,6 +11,7 @@ public class PR1QueueTest {
     PR1Queue pr1q;
 
     private void fillQueue() {
+        //aqui he modificado la longitud a 15, tal y como se ve en el documento pdf y cambiando los chars por integers
         for (Integer c = 0; c < 15; c++) {
             pr1q.add(c*c+3*c+2);
 
@@ -32,6 +33,7 @@ public class PR1QueueTest {
 
     @org.junit.Test
     public void queueTest() {
+        //Aqui he tenido que quitar -1 a this.pr1q.CAPACITy-1 porque si no me daba un error en valor esperado, ya que esperaba un valor menor al size
         assertEquals(this.pr1q.CAPACITY, this.pr1q.getQueue().size());
         //lo relleno con un for para ahorrar lineas de codigo
         for (int i =0; i<pr1q.CAPACITY ; i++) {
